258f9104119ae7ed8799fe16eed71feb0d2b7b16
WillChilds-Klein <willck93@gmail.com>
2024-03-26 21:43:05 +0000

    Build ruby against OSSL 1.0.2 and validate linkage

--- analyzer.py
+++ analyzer.py
@@ -27,6 +27,8 @@ def main():
     build_awslc()
     print("building openssl...")
     build_openssl_1_0_2()
+    print("biulding ruby against openssl...")
+    build_ruby()
     print("scanning symbols and sources...")
     openssl_libs = get_libs(os.path.join(REPOS_DIR, "openssl"))
     awslc_libs = get_libs(os.path.join(REPOS_DIR, "aws-lc"))
@@ -114,35 +116,112 @@ def fetch_source():
 
 def build_awslc():
     cmds = ["cmake -B build -DBUILD_SHARED_LIBS=ON", "make -C build"]
+    # TODO make install into local repo
     build_common("aws-lc", cmds)
 
 
 # TODO: create a Repo abc that requires users to provide a build method
 def build_openssl_1_0_2():
-    cmds = ["./config shared", "make"]
+    ossl_dir = os.path.abspath(f"{REPOS_DIR}/openssl/install")
+    cmds = [
+        f"./config --prefix={ossl_dir} --openssldir={ossl_dir}/openssl shared",
+        "make",
+        "make install",
+    ]
     build_common("openssl", cmds)
 
 
-def build_common(repo: str, cmds: list[str]):
+def build_ruby():
+    # TODO: patch ruby to disable TTS protocol + other unwanted stuff
+    ossl_dir = os.path.abspath(f"{REPOS_DIR}/openssl/install")
+    cmds = [
+        "./autogen.sh",
+        f"""\
+            ./configure --with-openssl-dir={ossl_dir} \
+                        --with-openssl-lib={ossl_dir}/lib \
+                        --with-openssl-include={ossl_dir}/include \
+        """.strip(),
+        "make",
+    ]
+    env = {
+        "CPPFLAGS": f"-I{ossl_dir}/include",
+        "LDFLAGS": f"-L{ossl_dir}/lib",
+    }
+    build_common("ruby", cmds, env=env)
+    ossl_lib = get_files(os.path.abspath(f"{REPOS_DIR}/ruby/.ext"), "openssl.so")
+    assert len(ossl_lib) == 1, ossl_lib
+    ossl_lib = next(iter(ossl_lib))
+    print("checking corect OpenSSL linkage...")
+    p1 = subprocess.Popen(
+        ["ldd", ossl_lib], cwd=f"{REPOS_DIR}/ruby", stdout=subprocess.PIPE
+    )
+    p2 = subprocess.check_call(
+        ["grep", f"{REPOS_DIR}/openssl/install/"],
+        cwd=f"{REPOS_DIR}/ruby",
+        stdin=p1.stdout,
+        stdout=subprocess.DEVNULL,
+    )
+    p1.wait()
+    print("checking corect OpenSSL version...")
+    p1 = subprocess.Popen(
+        [
+            "./ruby",
+            "-I.",
+            "-I.ext/x86_64-linux",
+            "-I./lib",
+            "-ropenssl",
+            "-e",
+            "'puts OpenSSL::OPENSSL_VERSION'",
+        ],
+        cwd=f"{REPOS_DIR}/ruby",
+        stdout=subprocess.PIPE,
+    )
+    p2 = subprocess.check_call(
+        ["cat", "-"],
+        # ["grep", "OpenSSL 1.0.2"],
+        cwd=f"{REPOS_DIR}/ruby",
+        stdin=p1.stdout,
+    )
+    p1.wait()
+
+    # validate OSSL:
+    #
+    #   $ ./ruby -I./ -I.ext/x86_64-linux/ -I./lib -ropenssl -e 'puts OpenSSL::OPENSSL_VERSION'
+    #   OpenSSL 1.0.2v-dev  xx XXX xxxx
+    #
+    #   $ ldd ./.ext/x86_64-linux/openssl.so
+    #   linux-vdso.so.1 (0x00007ffdf9bdc000)
+    #   libssl.so.1.0.0 => /home/ubuntu/symbol_analyzer/repos/openssl/install//lib/libssl.so.1.0.0 (0x00007f680fe9a000)
+    #   libcrypto.so.1.0.0 => /home/ubuntu/symbol_analyzer/repos/openssl/install//lib/libcrypto.so.1.0.0 (0x00007f680fc00000)
+    #   libc.so.6 => /lib/x86_64-linux-gnu/libc.so.6 (0x00007f680f800000)
+    #   /lib64/ld-linux-x86-64.so.2 (0x00007f680ff7a000)
+
+
+def build_common(repo: str, cmds: list[str], env: dict[str, str] = None):
+    subenv = dict(os.environ)
+    if env:
+        subenv = dict(subenv, **env)
     cwd = os.path.join(REPOS_DIR, repo)
     nproc = (
         subprocess.run("nproc", stdout=subprocess.PIPE).stdout.decode("UTF-8").strip()
     )
     for cmd in cmds:
         cmd_words = cmd.split(" ")
-        if cmd_words[0] == "make":
-            cmd_words.extend(["-j", nproc])
+        if cmd_words[0] == "make" and len(cmd_words) == 1:
+            cmd_words.insert(1, "-j")
+            cmd_words.insert(2, nproc)
         try:
             subprocess.run(
                 cmd_words,
                 check=True,
                 cwd=cwd,
+                env=subenv,
                 stdout=subprocess.DEVNULL,
                 stderr=subprocess.STDOUT,
             )
         except subprocess.CalledProcessError as e:
             print(f"WORK DIR: {cwd}")
-            print(f"COMMAND: {' && '.join(cmd_words)}")
+            print(f"COMMAND: {' '.join(cmd_words)}")
             print(f"STDERR: {e.stdout}")
             print(f"STDERR: {e.stderr}")
             raise e
